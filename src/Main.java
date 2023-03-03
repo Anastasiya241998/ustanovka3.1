88a9da74470544956edf0c562c3da861e0fe8c5a
Nastya Gracheva <nastya.nastya.gracheva.98.grach@mail.ru>
2023-03-04 00:27:58 +0300

    src2

--- src/Main.java
+++ src/Main.java
@@ -5,62 +5,51 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static String getLogMessage(String name, String path) {
-        String str = name.equals(" ") ? "по пути" + path + "был создан" : "error";
-
-        StringBuilder builder = new StringBuilder(str);
-        builder.append(str);
-        return builder.toString();
-
+    public static void createDirectorion (String divPath, StringBuilder stringBuilder) {
+        File dir = new File(divPath);
+        if(dir.mkdir())
+        stringBuilder.append(dir.getName() + "  каталог создан \n");
     }
 
-
-    private static void create(ArrayList<String> list, String path) {
-        File root = new File(path);
-        if (!root.exists()) {
-            try {
-                root.mkdir();
-                getLogMessage(" ", path);
-            } catch (Exception e) {
-                getLogMessage("", path);
-            }
-        }
-        for (String a : list) {
-            if (a.contains(".")) {
-                File file = new File(path + "//" + a);
-                try {
-                    file.createNewFile();
-                    getLogMessage(a, path);
-                } catch (Exception e) {
-                    getLogMessage(a, path);
-                }
-            } else {
-                File dir = new File(path + "//" + a);
-                try {
-                    dir.mkdir();
-                    getLogMessage(" ", path);
-                } catch (Exception e) {
-                    getLogMessage(" ", path);
-                }
-            }
+    public static void  createFile ( String filePath, String fileName, StringBuilder stringBuilder){
+        File file = new File(filePath, fileName);
+        try {
+            if (file.createNewFile())
+                stringBuilder.append(file.getName() + "  файл создан\n");
+        } catch (IOException ex){
+            System.out.println(ex.getMessage());
         }
     }
 
 
-    public static void main(String[] args) throws Exception {
 
-        create(new ArrayList<>(Arrays.asList("src", "res", "savegames", "temp")), "C:/Users/Анастасия/Desktop/Games");
-        create(new ArrayList<>(Arrays.asList("main", "test")), "C:/Users/Анастасия/Desktop/Games/src");
-        create(new ArrayList<>(Arrays.asList("Main.java", "Utils.java")), "C:/Users/Анастасия/Desktop/Games/src/main");
-        create(new ArrayList<>(Arrays.asList(
-                "drawables", "vectors", "icons"
-        )), "C:/Users/Анастасия/Desktop/Games/res");
-        create(new ArrayList<>(Arrays.asList(
-                "temp.txt"
-        )), "C:/Users/Анастасия/Desktop/Games/temp/temp");
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder();
+        String[] dirArray = new String[] { "C:/Users/Анастасия/Desktop/Games", "C:/Users/Анастасия/Desktop/Games/src", "C:/Users/Анастасия/Desktop/Games/res", "C:/Users/Анастасия/Desktop/Games/savegames",
+        "C:/Users/Анастасия/Desktop/Games/temp", "C:/Users/Анастасия/Desktop/Games/src/main", "C:/Users/Анастасия/Desktop/Games/src/test",
+        "C:/Users/Анастасия/Desktop/Games/res/drawables", "C:/Users/Анастасия/Desktop/Games/res/vectors", "C:/Users/Анастасия/Desktop/Games/res/icons"};
 
-        FileWriter writer = new FileWriter("C:/Users/Анастасия/Desktop/Games/temp/temp.txt");
-        writer.write(getLogMessage());
+        String[] fileNames = new String[] {"Main.java", "Utils.java", "temp.txt"};
+        String[] filePath = new String[] {"C:/Users/Анастасия/Desktop/Games/src/main", "C:/Users/Анастасия/Desktop/Games/src/main",
+                "C:/Users/Анастасия/Desktop/Games/temp/temp"};
+
+        for (String dir : dirArray) {
+            createDirectorion(dir, str);
+        }
+
+        for (int i = 0; i < fileNames.length; i++) {
+            createFile(filePath[i],fileNames[i], str);
+        }
+//
+        try (FileWriter writer  = new FileWriter("C:/Users/Анастасия/Desktop/Games/temp/temp.txt")) {
+            writer.write(str.toString());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+
+
+//
 
     }
 }
